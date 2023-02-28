24263d4958c5b3439820337fd867fd14e3db84da
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2023-02-28 18:22:59 +0500

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import "./App.css";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 import awsexports from "./aws-exports";
 Amplify.configure(awsexports);
 
-function App(props: any) {
-  console.log(props);
-
+function App({ signOut, user }: { signOut?: any; user?: any }) {
+  const { name, email, phone_number } = user.attributes;
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Test Result.</p>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phone_number}</p>
         <a
           className='App-link'
           href='https://reactjs.org'
@@ -23,6 +25,7 @@ function App(props: any) {
         >
           Test Result
         </a>
+        <button onClick={signOut}>Signout</button>
       </header>
     </div>
   );
